fc8528ed0d9d0381696c7cb53192208244cafbd4
chenzhenduo <chenzd_1993@163.com>
2022-06-12 17:43:56 +0800

    Delete train.sh

--- train.sh
+++
@@ -1,6 +0,0 @@
-python FISH.py --dataset cub_bird --code-length 32 --lr 0.01 --gpu-ids 6,7 --model-name resnet18 --class-mask 0.7
-#python FISH.py --dataset stanford_dog --code-length 32 --lr 0.001 --gpu-ids 6,7 --model-name resnet18 --class-mask 0.3
-#python FISH.py --dataset aircraft --code-length 32 --lr 0.01 --gpu-ids 6,7 --model-name resnet18 --class-mask 0.7
-#python FISH.py --dataset vegfru --code-length 32 --lr 0.01 --gpu-ids 6,7 --model-name resnet18 --class-mask 0.7
-
-
